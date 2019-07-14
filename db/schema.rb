e1c0de02e8c3348e19dcdf5748bc7cfff1f14b0d
v-crn <var.cyrano@gmail.com>
2019-07-14 23:29:19 +0900

    before exercise 6.2.5

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_160043) do
+ActiveRecord::Schema.define(version: 2019_07_14_140902) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
