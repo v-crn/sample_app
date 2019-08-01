5508e050a0bb0eb5e0accee5a7a0105a4868d135
v-crn <var.cyrano@gmail.com>
2019-08-01 15:56:27 +0900

    exercise 7.7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 														password_confirmation: "bar" }}
 		end
 		assert_template 'users/new'
+		assert_select 'div#error_explanation'
+		assert_select 'div.field_with_errors'
+		assert_select 'ul' do
+      assert_select 'li', 'Name can\'t be blank'
+      assert_select 'li', 'Email is invalid'
+      assert_select 'li', 'Password confirmation doesn\'t match Password'
+      assert_select 'li', 'Password is too short (minimum is 6 characters)'
+		end
 	end
 	
 	test "valid signup information" do
@@ -22,5 +30,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 			follow_redirect!
 		end
 		assert_template 'users/show'
+		assert_not flash.empty?
 	end
 end
